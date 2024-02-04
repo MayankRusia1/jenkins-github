9fe84304881875d9e3dd25bd1d137813394982be
Mayank Rusia <mayankrusia97@gmail.com>
2024-02-04 13:17:09 +0530

    Print java program files

---
+++ Print.java
@@ -0,0 +1,12 @@
+public class Print
+{
+public static void main(String[] args)
+{
+System.out.println("Hello and Welcome");
+
+for(int i=0; i< 5; i++)
+{
+System.out.println("Jai shree ram");
+}
+}
+}
